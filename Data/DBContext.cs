034efc1b92dfca00dc311e68bd26a6a6fc8c65bc
Hannah Hall <hannahkhall13@gmail.com>
2017-08-17 14:23:00 -0500

    Initial commit including dbcontext and project packages

---
+++ Data/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BangazonHR.Models;
+
+namespace BangazonHR.Data
+{
+    public class BangazonContext : DbContext
+    {
+        public BangazonContext(DbContextOptions<BangazonContext> options)
+            : base(options)
+        { }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonHR.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace BangazonHR
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+       public void ConfigureServices(IServiceCollection services)
+        {
+            Console.WriteLine("ConfigureServices");
+
+            // Add CORS framework
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowWhiteListOrigins",
+                    builder => builder.WithOrigins("http://example.com"));
+            });
+
+            // Add framework services.
+            services.AddMvc();
+
+
+            string path = System.Environment.GetEnvironmentVariable("BangazonHRDB");
+            var connection = $"Filename={path}";
+            Console.WriteLine($"connection = {connection}");
+            services.AddDbContext<BangazonContext>(options => options.UseSqlite(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
